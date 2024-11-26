409297624935710f8bb0ec9f34c976294b0a3e7e
Stanislav <stas89180305719@gmail.com>
2024-11-26 12:11:39 +0300

    edited the routers, added bot_cmd_list

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from handlers.user_private import user_private_router
 
 load_dotenv(find_dotenv())
 
+from handlers.user_private import user_private_router
+from common.bot_cmd_list import private
+
+
 ALLOWED_UPDATES = ['message', 'edited_message']
 
 
@@ -22,6 +26,8 @@ dp.include_router(user_private_router)
     
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
+    # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
+    await bot.set_my_commands(commands=private,scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
 asyncio.run(main())
