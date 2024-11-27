d2202d09468accf509260cb86cc573691b16639e
Stanislav <stas89180305719@gmail.com>
2024-11-27 17:28:43 +0300

    added user_group, chat_types

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 import asyncio
 import os
-
 from aiogram import Bot, Dispatcher, types
 from dotenv import find_dotenv, load_dotenv 
-from handlers.user_private import user_private_router
 
 load_dotenv(find_dotenv())
 
 from handlers.user_private import user_private_router
+from handlers.user_group import user_group_router
 from common.bot_cmd_list import private
 
 
-ALLOWED_UPDATES = ['message', 'edited_message']
+ALLOWED_UPDATES = ['message, edited_message']
 
 
 
@@ -20,13 +19,13 @@ dp = Dispatcher()
 
 
 dp.include_router(user_private_router)
+dp.include_router(user_group_router)
 
 
 
     
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
-    # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
     await bot.set_my_commands(commands=private,scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
